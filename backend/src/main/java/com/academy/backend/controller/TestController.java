c3ba994d19460e88f2fcf558f74a5ce0a22ca92e
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-01-17 10:46:01 +0900

    FEAT: Add POST method test api

--- backend/src/main/java/com/academy/backend/controller/TestController.java
+++ backend/src/main/java/com/academy/backend/controller/TestController.java
@@ -1,9 +1,7 @@
 package com.academy.backend.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +12,12 @@ public class TestController {
     @GetMapping("/cors")
     public ResponseEntity<?> getCorsTest() {
         CorsResponseDto responseDto = new CorsResponseDto("CORS GET Success", LocalDateTime.now());
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/cors")
+    public ResponseEntity<?> postCorsTest(@RequestParam String message) {
+        CorsResponseDto responseDto = new CorsResponseDto(message + " CORS POST Success", LocalDateTime.now());
         return ResponseEntity.ok(responseDto);
     }
 
