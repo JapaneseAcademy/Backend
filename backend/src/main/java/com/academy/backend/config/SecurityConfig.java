8f93b1fa66a671dd3df76310b5883311a3f8e84c
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-01-21 17:30:40 +0900

    FEAT: H2 관련 설정 추가

--- backend/src/main/java/com/academy/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/academy/backend/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -25,9 +26,10 @@ public class SecurityConfig {
                 .cors((SecurityConfig::corsAllow))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/api/v1/test/**").permitAll() // 여러 경로를 한 줄로 그룹화
+                                .requestMatchers("/h2-console/**", "/api/v1/test/**", "/api/v1/oauth/**").permitAll() // 여러 경로를 한 줄로 그룹화
                                 .anyRequest().authenticated() // 나머지 요청 인증 필요
-                );
+                )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
         return http.build();
     }
