f20b20c6254cbe6b8f4285e3ef5b1fbbd1c96a23
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-01-21 17:33:05 +0900

    FEAT: OAuth 컨트롤러 구현

---
+++ backend/src/main/java/com/academy/backend/controller/OAuthController.java
@@ -0,0 +1,29 @@
+package com.academy.backend.controller;
+
+import com.academy.backend.dto.request.oauth.KakaoOAuthRequest;
+import com.academy.backend.dto.response.oauth.LoginResponse;
+import com.academy.backend.service.member.MemberService;
+import com.academy.backend.service.oauth.KakaoOAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/oauth")
+@RequiredArgsConstructor
+public class OAuthController {
+
+    private final KakaoOAuthService kakaoOAuthService;
+    private final MemberService memberService;
+
+    @PostMapping("/kakao")
+    public ResponseEntity<?> kakaoLogin(@RequestBody KakaoOAuthRequest requestDto) {
+        String authorizationCode = requestDto.getAuthorizationCode();
+        LoginResponse loginResponse = kakaoOAuthService.kakaoLogin(authorizationCode);
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
