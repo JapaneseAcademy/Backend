dc355809a060e05673d9c73eab793fd39c9ea522
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-01-21 17:34:32 +0900

    FEAT: Member dto 작성

---
+++ backend/src/main/java/com/academy/backend/dto/response/MemberResponse.java
@@ -0,0 +1,34 @@
+package com.academy.backend.dto.response;
+
+import com.academy.backend.domain.member.Member;
+import com.academy.backend.domain.member.Role;
+import com.academy.backend.exception.member.UserMappingException;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class MemberResponse {
+
+    private Long id;
+    private String name;
+    private String phone;
+    private LocalDate birth;
+    private Role role;
+
+    public static MemberResponse of(Member member) {
+        try {
+            return MemberResponse.builder()
+                    .id(member.getId())
+                    .name(member.getName())
+                    .phone(member.getPhone())
+                    .birth(member.getBirth())
+                    .role(member.getRole())
+                    .build();
+        } catch (Exception e) {
+            throw new UserMappingException();
+        }
+    }
+}
