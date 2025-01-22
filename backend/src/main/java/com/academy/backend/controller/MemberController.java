4d2b86a966cb356c38e63d0f709509153f244eac
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-01-23 00:20:38 +0900

    FIX: JoinResponse import문 삭제

--- backend/src/main/java/com/academy/backend/controller/MemberController.java
+++ backend/src/main/java/com/academy/backend/controller/MemberController.java
@@ -1,14 +1,6 @@
 package com.academy.backend.controller;
 
-import com.academy.backend.dto.request.JoinRequest;
-import com.academy.backend.dto.response.JoinResponse;
-import com.academy.backend.dto.response.oauth.LoginResponse;
-import com.academy.backend.service.member.MemberService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
