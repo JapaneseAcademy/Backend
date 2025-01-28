d0b057bd81fb24908d3e2a4e3b7415c775f31588
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-01-28 22:18:24 +0900

    FEAT: 새로운 사용자 여부(requiresSignUp) 필드 추가

--- backend/src/main/java/com/academy/backend/dto/response/oauth/LoginResponse.java
+++ backend/src/main/java/com/academy/backend/dto/response/oauth/LoginResponse.java
@@ -1,19 +1,39 @@
 package com.academy.backend.dto.response.oauth;
 
+import com.academy.backend.domain.member.Member;
 import com.academy.backend.domain.member.Role;
 import com.academy.backend.dto.jwt.AuthToken;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class LoginResponse {
 
     private Long id;
+    private String loginId;
     private String name;
     private Role role;
     private boolean isActive;
     private AuthToken token;
+    private boolean requiresSignUp;
+
+    public LoginResponse(Member member, AuthToken token, boolean requiresSignUp) {
+        this.id = member.getId();
+        this.loginId = member.getLoginId();
+        this.name = member.getName();
+        this.role = member.getRole();
+        this.isActive = member.isActive();
+        this.token = token;
+        this.requiresSignUp = requiresSignUp;
+    }
+
+    public LoginResponse(String loginId, boolean requiresSignUp) {
+        this.loginId = loginId;
+        this.requiresSignUp = requiresSignUp;
+    }
 
     public LoginResponse(Long id, String name, Role role, boolean isActive, AuthToken token) {
         this.id = id;
