5b2d2a650299e6eaa2c2978b120d77bc8c580bd6
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-01-28 22:16:32 +0900

    STYLE: 공백을 _으로 대체

--- backend/src/main/java/com/academy/backend/exception/oauth/KakaoTokenResponseException.java
+++ backend/src/main/java/com/academy/backend/exception/oauth/KakaoTokenResponseException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class KakaoTokenResponseException extends BaseException {
     public KakaoTokenResponseException() {
-        super("Kakao information with token failed", "KAKAO TOKEN ERROR", HttpStatus.BAD_REQUEST);
+        super("Kakao information with token failed", "KAKAO_TOKEN_ERROR", HttpStatus.BAD_REQUEST);
     }
 }
