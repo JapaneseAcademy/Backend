1bb1b1677625df8e94a8e874226398ec2454ce1d
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-01-29 21:01:23 +0900

    FEAT: course 엔티티 등록

---
+++ backend/src/main/java/com/academy/backend/domain/Course.java
@@ -0,0 +1,39 @@
+package com.academy.backend.domain;
+
+import com.academy.backend.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "courseId")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String description;
+
+    @Builder
+    public Course(Member member, String title, String description) {
+        this.member = member;
+        this.title = title;
+        this.description = description;
+    }
+}
