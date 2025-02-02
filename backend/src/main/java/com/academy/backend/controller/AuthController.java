1665779056cef0837c878a69d349b19f6d4eec80
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-03 00:45:07 +0900

    FEAT: 토큰 재발급 api 작성

--- backend/src/main/java/com/academy/backend/controller/AuthController.java
+++ backend/src/main/java/com/academy/backend/controller/AuthController.java
@@ -2,16 +2,17 @@ package com.academy.backend.controller;
 
 import com.academy.backend.dto.request.JoinRequest;
 import com.academy.backend.dto.request.oauth.KakaoOAuthRequest;
+import com.academy.backend.dto.request.oauth.RefreshTokenRequest;
 import com.academy.backend.dto.response.oauth.LoginResponse;
+import com.academy.backend.dto.response.oauth.RefreshTokenResponse;
 import com.academy.backend.service.member.MemberService;
 import com.academy.backend.service.oauth.KakaoOAuthService;
+import com.academy.backend.service.oauth.RefreshTokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -20,6 +21,7 @@ public class AuthController {
 
     private final MemberService memberService;
     private final KakaoOAuthService kakaoOAuthService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/members")
     public ResponseEntity<LoginResponse> joinMember(@Valid @RequestBody JoinRequest request) {
@@ -29,9 +31,16 @@ public class AuthController {
     }
 
     @PostMapping("/kakao")
-    public ResponseEntity<?> kakaoLogin(@RequestBody KakaoOAuthRequest request) {
+    public ResponseEntity<LoginResponse> kakaoLogin(@RequestBody KakaoOAuthRequest request) {
         LoginResponse loginResponse = kakaoOAuthService.kakaoLogin(request);
 
         return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<RefreshTokenResponse> refresh(@RequestHeader("Authorization") String authorizationHeader, @Valid @RequestBody RefreshTokenRequest request) {
+        RefreshTokenResponse response = refreshTokenService.refresh(authorizationHeader, request);
+
+        return ResponseEntity.ok(response);
     }
 }
