01da40190c8afce011509f6ed67439d6df797c8b
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-24 17:08:18 +0900

    FEAT: 리뷰 생성, 조회 api

--- backend/src/main/java/com/academy/backend/controller/ReviewController.java
+++ backend/src/main/java/com/academy/backend/controller/ReviewController.java
@@ -1,12 +1,11 @@
 package com.academy.backend.controller;
 
-import com.academy.backend.dto.request.enrollment.ReviewCreateRequest;
-import com.academy.backend.dto.response.enrollment.ReviewResponse;
+import com.academy.backend.dto.request.review.ReviewCreateRequest;
+import com.academy.backend.dto.response.review.ReviewListResponse;
+import com.academy.backend.dto.response.review.ReviewResponse;
 import com.academy.backend.service.review.ReviewService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,25 +23,21 @@ public class ReviewController {
     public ResponseEntity<?> createReview(
             @RequestHeader("Authorization") String authorizationHeader,
             @RequestPart("request") ReviewCreateRequest request,
-            @RequestPart("image") MultipartFile image
+            @RequestPart(value = "images", required = false) List<MultipartFile> images
             ) {
-        reviewService.createReview(authorizationHeader, request, image);
+        reviewService.createReview(authorizationHeader, request, images);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("/courses/{courseId}")
-    public ResponseEntity<List<ReviewResponse>> getReviewsByCourseId(@RequestHeader("Authorization") String authorizationHeader, @PathVariable Long courseId) {
-        List<ReviewResponse> responses = reviewService.getReviewsByCourseId(courseId);
+    @GetMapping("")
+    public ResponseEntity<ReviewListResponse> getReviewsByCourseId(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @RequestParam("courseId") Long courseId,
+            @RequestParam(value = "page", defaultValue = "0") Integer page
+            ) {
+        ReviewListResponse responses = reviewService.getReviewsByCourseId(page, courseId);
 
         return ResponseEntity.ok(responses);
-    }
-
-    @PostMapping("/tt")
-    public ResponseEntity<?> testImage(@RequestPart("image") MultipartFile image) {
-        if (image != null) {
-            System.out.println(image);
-        }
-        return ResponseEntity.ok("test success");
     }
 }
