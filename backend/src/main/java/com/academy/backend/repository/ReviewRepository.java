2a4fe941374bf2ad41de7c8b4ab084015ae75b1f
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-24 17:07:08 +0900

    FEAT: 리뷰 조회 로직 작성

--- backend/src/main/java/com/academy/backend/repository/ReviewRepository.java
+++ backend/src/main/java/com/academy/backend/repository/ReviewRepository.java
@@ -1,6 +1,9 @@
 package com.academy.backend.repository;
 
 import com.academy.backend.domain.review.Review;
+import com.academy.backend.dto.response.review.BestReviewResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +14,19 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("SELECT r FROM Review r JOIN FETCH r.enrollment e WHERE e.course.id = :courseId")
-    List<Review> findByCourseId(@Param("courseId") Long courseId);
+    @Query("SELECT r " +
+            "FROM Review r " +
+            "LEFT JOIN r.enrollment e " +
+            "LEFT JOIN FETCH r.reviewImages ri " +
+            "WHERE e.course.id = :courseId " +
+            "AND r.isVisible = true")
+    Page<Review> findByCourseId(@Param("courseId") Long courseId, Pageable pageable);
+
+    @Query("SELECT new com.academy.backend.dto.response.review.BestReviewResponse" +
+            "(c.title, r.title, r.review, r.name, ri.imageUrl, r.isAnonymous, r.createdAt)")
+    List<BestReviewResponse> findBestReviews();
+
+    boolean existsByEnrollmentId(Long enrollmentId);
+
+    Page<Review> findAll(Pageable pageable);
 }
