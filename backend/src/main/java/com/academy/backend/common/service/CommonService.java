76f723f0d41dad95fad73164b011d5a432e65cb7
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-25 15:47:21 +0900

    CHORE: 공통 로직 분리(review 조회)

--- backend/src/main/java/com/academy/backend/common/service/CommonService.java
+++ backend/src/main/java/com/academy/backend/common/service/CommonService.java
@@ -3,6 +3,9 @@ package com.academy.backend.common.service;
 import com.academy.backend.enrollment.domain.Enrollment;
 import com.academy.backend.enrollment.repository.EnrollmentRepository;
 import com.academy.backend.exception.enrollment.EnrollmentNotFoundException;
+import com.academy.backend.exception.review.ReviewNotFoundException;
+import com.academy.backend.review.domain.Review;
+import com.academy.backend.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +15,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommonService {
 
     private final EnrollmentRepository enrollmentRepository;
+    private final ReviewRepository reviewRepository;
 
     @Transactional(readOnly = true)
     public Enrollment getEnrollmentByEnrollmentId(Long enrollmentId) {
-         return enrollmentRepository.findById(enrollmentId)
-                 .orElseThrow(() -> new EnrollmentNotFoundException(enrollmentId));
+        return enrollmentRepository.findById(enrollmentId)
+                .orElseThrow(() -> new EnrollmentNotFoundException(enrollmentId));
+    }
+
+    @Transactional(readOnly = true)
+    public Review getReviewByReviewId(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ReviewNotFoundException(reviewId));
     }
 }
