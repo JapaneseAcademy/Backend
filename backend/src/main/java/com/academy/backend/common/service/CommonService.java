dcc6b4e409d12f4b271fcc811849d3f0abe1cd92
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-25 16:56:00 +0900

    REFACTOR: 공통 로직 분리(Member 조회)

--- backend/src/main/java/com/academy/backend/common/service/CommonService.java
+++ backend/src/main/java/com/academy/backend/common/service/CommonService.java
@@ -3,7 +3,10 @@ package com.academy.backend.common.service;
 import com.academy.backend.enrollment.domain.Enrollment;
 import com.academy.backend.enrollment.repository.EnrollmentRepository;
 import com.academy.backend.exception.enrollment.EnrollmentNotFoundException;
+import com.academy.backend.exception.member.UserNotFoundException;
 import com.academy.backend.exception.review.ReviewNotFoundException;
+import com.academy.backend.member.domain.Member;
+import com.academy.backend.member.repository.MemberRepository;
 import com.academy.backend.review.domain.Review;
 import com.academy.backend.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +17,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CommonService {
 
+    private final MemberRepository memberRepository;
     private final EnrollmentRepository enrollmentRepository;
     private final ReviewRepository reviewRepository;
+
+    @Transactional(readOnly = true)
+    public Member getMemberByMemberId(Long id) {
+        return memberRepository.findById(id).orElseThrow(
+                () -> new UserNotFoundException(id)
+        );
+    }
 
     @Transactional(readOnly = true)
     public Enrollment getEnrollmentByEnrollmentId(Long enrollmentId) {
