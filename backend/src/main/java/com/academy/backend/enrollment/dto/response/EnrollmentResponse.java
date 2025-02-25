218d466147b78f6e1e489ea44ed9fdd061c7eb12
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-25 10:38:55 +0900

    CHORE: course Title 반환 필드 추가

--- backend/src/main/java/com/academy/backend/enrollment/dto/response/EnrollmentResponse.java
+++ backend/src/main/java/com/academy/backend/enrollment/dto/response/EnrollmentResponse.java
@@ -15,26 +15,10 @@ public class EnrollmentResponse {
     private Long id;
     private Long memberId;
     private Long courseId;
+    private String courseTitle;
     private Category category;
     private Integer paymentAmount;
     private LocalDateTime paymentAt;
     private LocalDate startDate;
     private LocalDate endDate;
-
-    public static EnrollmentResponse of(Enrollment enrollment) {
-        try {
-            return EnrollmentResponse.builder()
-                    .id(enrollment.getId())
-                    .memberId(enrollment.getMember().getId())
-                    .courseId(enrollment.getCourse().getId())
-                    .category(enrollment.getCategory())
-                    .paymentAmount(enrollment.getPaymentAmount())
-                    .paymentAt(enrollment.getPaymentAt())
-                    .startDate(enrollment.getStartDate())
-                    .endDate(enrollment.getEndDate())
-                    .build();
-        } catch (Exception e) {
-            throw new RuntimeException("wrong mapping");
-        }
-    }
 }
