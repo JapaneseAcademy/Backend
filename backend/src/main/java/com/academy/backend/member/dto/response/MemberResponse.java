a750a65a0740d0240c8ec45f7af4bafaa6662baa
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-25 16:51:11 +0900

    CHORE: Member 프로필 응답 dto 수정

--- backend/src/main/java/com/academy/backend/member/dto/response/MemberResponse.java
+++ backend/src/main/java/com/academy/backend/member/dto/response/MemberResponse.java
@@ -1,8 +1,5 @@
 package com.academy.backend.member.dto.response;
 
-import com.academy.backend.member.domain.Member;
-import com.academy.backend.member.domain.Role;
-import com.academy.backend.exception.member.UserMappingException;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -16,19 +13,4 @@ public class MemberResponse {
     private String name;
     private String phone;
     private LocalDate birth;
-    private Role role;
-
-    public static MemberResponse of(Member member) {
-        try {
-            return MemberResponse.builder()
-                    .id(member.getId())
-                    .name(member.getName())
-                    .phone(member.getPhone())
-                    .birth(member.getBirth())
-                    .role(member.getRole())
-                    .build();
-        } catch (Exception e) {
-            throw new UserMappingException();
-        }
-    }
 }
