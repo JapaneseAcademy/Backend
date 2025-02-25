8832b8a6fe9deb4a0abfc1b6b72bdc70f4275411
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-25 15:31:03 +0900

    FEAT: Enrollment 상세 정보 변환 로직 구현

--- backend/src/main/java/com/academy/backend/enrollment/converter/EnrollmentConverter.java
+++ backend/src/main/java/com/academy/backend/enrollment/converter/EnrollmentConverter.java
@@ -1,15 +1,20 @@
 package com.academy.backend.enrollment.converter;
 
 import com.academy.backend.enrollment.domain.Enrollment;
+import com.academy.backend.enrollment.dto.response.EnrollmentDetailResponse;
 import com.academy.backend.enrollment.dto.response.EnrollmentResponse;
 import com.academy.backend.exception.enrollment.EnrollmentMappingException;
+import com.academy.backend.review.converter.ReviewConverter;
+import com.academy.backend.review.domain.Review;
+
+import java.util.Optional;
 
 public class EnrollmentConverter {
 
-    public static EnrollmentResponse toEnrollmentResponse(Enrollment enrollment) {
+    public static EnrollmentResponse toEnrollmentResponse(Enrollment enrollment, Boolean reviewed) {
         try {
             return EnrollmentResponse.builder()
-                    .id(enrollment.getId())
+                    .enrollmentId(enrollment.getId())
                     .memberId(enrollment.getMember().getId())
                     .courseId(enrollment.getCourse().getId())
                     .courseTitle(enrollment.getCourse().getTitle())
@@ -18,6 +23,28 @@ public class EnrollmentConverter {
                     .paymentAt(enrollment.getPaymentAt())
                     .startDate(enrollment.getStartDate())
                     .endDate(enrollment.getEndDate())
+                    .reviewed(reviewed)
+                    .build();
+        } catch (Exception e) {
+            throw new EnrollmentMappingException();
+        }
+    }
+
+    public static EnrollmentDetailResponse toEnrollmentDetailResponse(Enrollment enrollment, Review review) {
+
+        try {
+            return EnrollmentDetailResponse.builder()
+                    .enrollmentId(enrollment.getId())
+                    .courseId(enrollment.getCourse().getId())
+                    .courseTitle(enrollment.getCourse().getTitle())
+                    .category(enrollment.getCategory())
+                    .paymentAmount(enrollment.getPaymentAmount())
+                    .paymentAt(enrollment.getPaymentAt())
+                    .startDate(enrollment.getStartDate())
+                    .endDate(enrollment.getEndDate())
+                    .reviewResponse(Optional.ofNullable(review)
+                                        .map(ReviewConverter::toReviewResponse)
+                                        .orElse(null))
                     .build();
         } catch (Exception e) {
             throw new EnrollmentMappingException();
