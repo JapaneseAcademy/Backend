30d23dff472641cf43e2734614ae37950da0c854
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-02-28 15:02:34 +0900

    CHORE: Course 조회 시 삭제 처리되지 않은 것만 조회(isActive = true)

--- backend/src/main/java/com/academy/backend/course/repository/CourseRepository.java
+++ backend/src/main/java/com/academy/backend/course/repository/CourseRepository.java
@@ -2,9 +2,22 @@ package com.academy.backend.course.repository;
 
 import com.academy.backend.course.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    @Query("SELECT c " +
+            "FROM Course c " +
+            "WHERE c.isActive = true and c.id = :courseId")
+    Optional<Course> findById(Long courseId);
+
+    @Query("SELECT c " +
+            "FROM Course c " +
+            "WHERE c.isActive = true")
+    List<Course> findAll();
 }
